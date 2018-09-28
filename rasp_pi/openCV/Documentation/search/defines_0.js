187816efb326112644e3c6e7d5adace24d4c5487
kavanomo <frachelzhang@gmail.com>
2018-09-28 16:09:24 -0400

    Initial Commit - Adding OpenCV (Hopefully the right files lmao)

---
+++ rasp_pi/openCV/Documentation/search/defines_0.js
@@ -0,0 +1,19 @@
+var searchData=
+[
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5favx',['__CV_CPU_DISPATCH_CHAIN_AVX',['../d2/d3b/cv__cpu__helper_8h.html#a18d9dbfa4d7070e2cf567fea67f63e80',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5favx2',['__CV_CPU_DISPATCH_CHAIN_AVX2',['../d2/d3b/cv__cpu__helper_8h.html#a9598c037548c392c3f87ee7df755d3fc',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5favx512_5fskx',['__CV_CPU_DISPATCH_CHAIN_AVX512_SKX',['../d2/d3b/cv__cpu__helper_8h.html#a4d55ae21bdd8a8d484bee238a83dc492',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5favx_5f512f',['__CV_CPU_DISPATCH_CHAIN_AVX_512F',['../d2/d3b/cv__cpu__helper_8h.html#ad837f805e9cfba74e9bd1d26e8008511',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fbaseline',['__CV_CPU_DISPATCH_CHAIN_BASELINE',['../d2/d3b/cv__cpu__helper_8h.html#aaec1a65f7dac82391f8a4d210dc63770',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5ffma3',['__CV_CPU_DISPATCH_CHAIN_FMA3',['../d2/d3b/cv__cpu__helper_8h.html#ac01ebf8b88fa6adc3689db906ad9daaf',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5ffp16',['__CV_CPU_DISPATCH_CHAIN_FP16',['../d2/d3b/cv__cpu__helper_8h.html#a75d63d8491d02d205cfb7bb7a983619c',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fneon',['__CV_CPU_DISPATCH_CHAIN_NEON',['../d2/d3b/cv__cpu__helper_8h.html#aa57096647ab7ffdfb4b8ea7d5b62b761',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fpopcnt',['__CV_CPU_DISPATCH_CHAIN_POPCNT',['../d2/d3b/cv__cpu__helper_8h.html#a6c507b9192617b05cf389702dcf15398',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fsse',['__CV_CPU_DISPATCH_CHAIN_SSE',['../d2/d3b/cv__cpu__helper_8h.html#ab706e0d1a4ea8c572145926355cc3758',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fsse2',['__CV_CPU_DISPATCH_CHAIN_SSE2',['../d2/d3b/cv__cpu__helper_8h.html#ad6c9cccd635190cd6bd19676cde77541',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fsse3',['__CV_CPU_DISPATCH_CHAIN_SSE3',['../d2/d3b/cv__cpu__helper_8h.html#a62564e6d6f6eaba2f8aced288573da0c',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fsse4_5f1',['__CV_CPU_DISPATCH_CHAIN_SSE4_1',['../d2/d3b/cv__cpu__helper_8h.html#ac1d8dbfd84b7ca2bfb57b357b8035b09',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fsse4_5f2',['__CV_CPU_DISPATCH_CHAIN_SSE4_2',['../d2/d3b/cv__cpu__helper_8h.html#a821885141776bee295e36b53a7451ed8',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fssse3',['__CV_CPU_DISPATCH_CHAIN_SSSE3',['../d2/d3b/cv__cpu__helper_8h.html#a2b6afa84a39ab34eee583abf228cda10',1,'cv_cpu_helper.h']]],
+  ['_5f_5fcv_5fcpu_5fdispatch_5fchain_5fvsx',['__CV_CPU_DISPATCH_CHAIN_VSX',['../d2/d3b/cv__cpu__helper_8h.html#a1947a99bfbca6418c164f3d2f8ae25da',1,'cv_cpu_helper.h']]]
+];
---
+++ rasp_pi/openCV/Documentation/search/enumvalues_18.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['x_5frow',['X_ROW',['../da/d44/classcv_1_1cuda_1_1ORB.html#a697f39a5afa598cce28e1d43a9e77428a541b9f9483d2fe06195df06da6ead753',1,'cv::cuda::ORB::X_ROW()'],['../db/d06/classcv_1_1cuda_1_1SURF__CUDA.html#a3b5156553e52661b04bf02fa9628f746a8b8055f3bc99ae13c5dd3c8cbe2bf0dc',1,'cv::cuda::SURF_CUDA::X_ROW()']]],
+  ['xyz',['XYZ',['../d3/ddc/group__ccalib.html#gga025ad7582700972413107e35b6b4182ea1b9784248615a43b52020905cebd78f8',1,'cv::omnidir']]],
+  ['xyzrgb',['XYZRGB',['../d3/ddc/group__ccalib.html#gga025ad7582700972413107e35b6b4182eaa1adad1b0fe06ed7c8203712c194aa0b',1,'cv::omnidir']]]
+];
